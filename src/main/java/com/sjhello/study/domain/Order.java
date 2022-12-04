f38b210fcf8a8d3c66de9b437175fa23051c45f3
sjhello <sjhello235@gmail.com>
2022-12-05 00:16:00 +0900

    refactor: 상품이 출고되었는지 확인하는 메서드 추가

--- src/main/java/com/sjhello/study/domain/Order.java
+++ src/main/java/com/sjhello/study/domain/Order.java
@@ -4,17 +4,16 @@ public class Order {
 
 	private OrderState orderState;
 	private ShippingInfo shippingInfo;
-
+	
 	public void changeShippingInfo(ShippingInfo shippingInfo) {
-		if(!isChangeable()) {
-			throw new IllegalArgumentException("배송지 정보를 변경 할 수 없습니다");
-		}
+		isNotYetShipped();
 		this.shippingInfo = shippingInfo;
 	}
 
-	private boolean isChangeable() {
-		return this.orderState == OrderState.PREPARING
-			|| this.orderState == OrderState.PAYMENT_WAITING;
+	private void isNotYetShipped() {
+		if (this.orderState != OrderState.PREPARING && this.orderState != OrderState.PAYMENT_WAITING) {
+			throw new IllegalArgumentException("상품이 이미 출고 되었습니다");
+		}
 	}
 
 }

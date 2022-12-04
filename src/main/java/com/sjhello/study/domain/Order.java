2b7ce3a7eb692f8ba6d7e60157a11073f5f2e0ab
sjhello <sjhello235@gmail.com>
2022-12-05 00:38:21 +0900

    refactor: 상품출고 확인 메서드 이름 변경

--- src/main/java/com/sjhello/study/domain/Order.java
+++ src/main/java/com/sjhello/study/domain/Order.java
@@ -6,16 +6,16 @@ public class Order {
 	private ShippingInfo shippingInfo;
 
 	public void cancel() {
-		isNotYetShipped();
+		verifyNotYetShipped();
 		this.orderState = OrderState.CANCEL;
 	}
 
 	public void changeShippingInfo(ShippingInfo shippingInfo) {
-		isNotYetShipped();
+		verifyNotYetShipped();
 		this.shippingInfo = shippingInfo;
 	}
 
-	private void isNotYetShipped() {
+	private void verifyNotYetShipped() {
 		if (this.orderState != OrderState.PREPARING && this.orderState != OrderState.PAYMENT_WAITING) {
 			throw new IllegalArgumentException("상품이 이미 출고 되었습니다");
 		}

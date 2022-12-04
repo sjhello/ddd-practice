5a906aeced6118fe4f2cc5820d16535b95de6ec2
sjhello <sjhello235@gmail.com>
2022-12-05 00:29:07 +0900

    feat: 상품, 주문정보 도메인 생성

---
+++ src/main/java/com/sjhello/study/domain/OrderInfo.java
@@ -0,0 +1,9 @@
+package com.sjhello.study.domain;
+
+import java.util.List;
+
+public class OrderInfo {
+
+	private List<Product> products;
+	private int totalPrice;
+}
---
+++ src/main/java/com/sjhello/study/domain/Product.java
@@ -0,0 +1,8 @@
+package com.sjhello.study.domain;
+
+public class Product {
+
+	private String name;
+	private int price;
+
+}

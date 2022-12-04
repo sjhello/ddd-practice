e2b842528a9ac35b1a633ac907008232d32da5ab
sjhello <sjhello235@gmail.com>
2022-12-05 00:06:36 +0900

    feat: 상품의 상태가 출고, 배송중, 배송 완료이면 배송지 정보를 변경할 수 없다

--- src/main/java/com/sjhello/study/domain/Order.java
+++ src/main/java/com/sjhello/study/domain/Order.java
@@ -5,6 +5,16 @@ public class Order {
 	private OrderState orderState;
 	private ShippingInfo shippingInfo;
 
+	public void changeShippingInfo(ShippingInfo shippingInfo) {
+		if(!isChangeable()) {
+			throw new IllegalArgumentException("배송지 정보를 변경 할 수 없습니다");
+		}
+		this.shippingInfo = shippingInfo;
+	}
 
+	private boolean isChangeable() {
+		return this.orderState == OrderState.PREPARING
+			|| this.orderState == OrderState.PAYMENT_WAITING;
+	}
 
 }

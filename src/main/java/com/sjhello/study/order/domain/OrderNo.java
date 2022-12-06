1b898d670a7574eea345f360d3d03164bbf7c977
sjhello <sjhello235@gmail.com>
2022-12-06 15:57:55 +0900

    feat: 주문 번호 생성

---
+++ src/main/java/com/sjhello/study/order/domain/OrderNo.java
@@ -0,0 +1,14 @@
+package com.sjhello.study.order.domain;
+
+public class OrderNo {
+
+	private String orderNo;
+
+	public OrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	public String getOrderNo() {
+		return orderNo;
+	}
+}
